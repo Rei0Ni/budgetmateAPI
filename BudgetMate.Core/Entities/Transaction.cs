4bc19ed03c1785dd592740ad9e9248a311b497c9
Abdelrahman Hamdy <abdelrahman.hamdy.hashim@gmail.com>
2024-09-05 18:56:45 +0300

    new file:   BudgetMate.Core/Entities/Transaction.cs 	modified:   BudgetMate.Core/obj/Debug/net8.0/BudgetMate.Core.AssemblyInfo.cs 	modified:   BudgetMate.Core/obj/Debug/net8.0/BudgetMate.Core.AssemblyInfoInputs.cache

---
+++ BudgetMate.Core/Entities/Transaction.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace BudgetMate.Core.Entities;
+
+public class Transaction
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public TransactionType Type { get; set; }
+    public decimal amount { get; set; }
+    public TransactionCategory Category { get; set; }
+    public string Description { get; set; } = "";
+    public DateTime Date { get; set; }
+}
+
+public enum TransactionType
+{
+    INCOME = 0,
+    EXPENSE = 1
+}
+
+public enum TransactionCategory
+{
+    INCOME_SALARY = 0,
+    INCOME_BONUS =1,
+    INCOME_FREELANCING = 2,
+    INCOME_INVESTMENT_GAINS = 3,
+    INCOME_RENTAL_INCOME = 4,
+    INCOME_REFUNDS = 5,
+    INCOME_GRANT = 6,
+
+    EXPENSE_RENT = 7,
+    EXPENSE_UTILITIES = 8,
+    EXPENSE_GROCERIES = 9,
+    EXPENSE_TRANSPORTATION = 10,
+    EXPENSE_INSURANCE = 11,
+    EXPENSE_HEALTHCARE = 12,
+    EXPENSE_ENTERTAINMENT = 13,
+    EXPENSE_SHOPPING = 14,
+    EXPENSE_DEPT_PAYMENT = 15,
+    EXPENSE_INVESTMENT = 16,
+    EXPENSE_TAXES = 17,
+    EXPENSE_DONATION = 18,
+}
--- BudgetMate.Core/obj/Debug/net8.0/BudgetMate.Core.AssemblyInfo.cs
+++ BudgetMate.Core/obj/Debug/net8.0/BudgetMate.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetMate.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9463ae0cf81a16afd76e59355e4b791d913a3c11")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetMate.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetMate.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
